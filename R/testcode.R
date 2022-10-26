954bd9466cfb259c9b68011abbf0c7e367a4809f
Seb Bate <86954917+sgbstats@users.noreply.github.com>
2022-10-26 12:42:51 +0100

    Clean

--- R/testcode.R
+++ R/testcode.R
@@ -6,7 +6,8 @@ library(openxlsx)
 library(beepr)
 library(chron)
 library(tibble)
-
+library(chron)
+library(googlesheets4)
 adapat=tribble(~Name, ~Barcode,
                "Seb", 493595,
                "Rachel", 1539187,
@@ -23,9 +24,10 @@ adapat=tribble(~Name, ~Barcode,
                "Leah", 5700545,
                "Grace", 7554791,
                "Ella", 7470572,
-               "Tom A", 5243446
+               "Tom A", 5243446,
+               "Phil", 7266141
 )%>% arrange(Name) %>%  mutate(rm=paste("|", Name, "|[A",Barcode,"](https://www.parkrun.org.uk/parkrunner/",Barcode,"/all/)|",sep=""),
-                                         url=paste("https://www.parkrun.org.uk/parkrunner/",Barcode,"/all/",sep=""))
+                               url=paste("https://www.parkrun.org.uk/parkrunner/",Barcode,"/all/",sep=""))
 
 
 
@@ -37,14 +39,29 @@ parkruns=tribble(~Name,~Event,~`Run Date`,~`Run Number`,~Pos,~Time,~AgeGrade,~`P
 for(i in 1:nrow(adapat))
 {
   url=adapat$url[i]
-link=getURL(url)         
-table=readHTMLTable(link)[[3]] %>% mutate(Name=adapat$Name[i])
-parkruns=parkruns %>% rbind.data.frame(table)
-Sys.sleep(10)
+  link=getURL(url)         
+  table=readHTMLTable(link)[[3]] %>% mutate(Name=adapat$Name[i])
+  parkruns=parkruns %>% rbind.data.frame(table)
+  Sys.sleep(10)
 }
+beepr::beep()
 
-parkruns %>% count(Event) %>% arrange(-n)
+parkruns=parkruns %>% mutate(Time2=case_when(str_length(Time)==5~chron(times=paste("00:",Time, sep="")),
+                                             T~chron(times=paste(Time))))
+parkruns %>% count(Name, Event) %>% arrange(-n) %>% count(Name)
 
 x=parkruns %>% count(Name, Event) %>% pivot_wider(names_from = "Name", values_from = n) %>% 
   merge(parkruns %>% count(Event))%>% arrange(-n) %>% select(-n)
 
+
+sheet_write(x, ss="https://docs.google.com/spreadsheets/d/1Bv_LGrOK6leEFV76OlHPsLX8CvM17CmEhQ2_JzZg5YI/edit#gid=0",
+            sheet="Parkruns")
+
+y=parkruns %>% group_by(Name, Event) %>% slice_min(Time2, with_ties = F) %>% 
+  mutate(Time=substr(as.character(Time2), 9-str_length(Time),9)) %>% 
+  dplyr::select(Name, Event, Time) %>% 
+  pivot_wider(names_from = "Name", values_from = Time) %>% 
+  merge(parkruns %>% count(Event))%>% arrange(-n) %>% select(-n)
+
+sheet_write(y, ss="https://docs.google.com/spreadsheets/d/1Bv_LGrOK6leEFV76OlHPsLX8CvM17CmEhQ2_JzZg5YI/edit#gid=0",
+            sheet="PBs")
