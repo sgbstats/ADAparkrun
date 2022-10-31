95e1ab7523889d96dd7aad130fda04024c9d2604
Seb Bate <86954917+sgbstats@users.noreply.github.com>
2022-10-31 14:45:46 +0000

    MVP updates (#2)
    * New shiny
    
    * MVP updates

---
+++ ADAparkrun/app.R
@@ -0,0 +1,147 @@
+
+library(shiny)
+library(XML)
+library(tidyverse)
+library(RCurl)
+library(shinycssloaders)
+library(googlesheets4)
+load("Data.Rda")
+load("adapat.Rda")
+`%notin%`=Negate(`%in%`)
+options(gargle_oauth_cache = ".secrets")
+gs4_auth(
+  cache = ".secrets",
+  email = "sebastiangbate@gmail.com"
+)
+parkruns=googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1Bv_LGrOK6leEFV76OlHPsLX8CvM17CmEhQ2_JzZg5YI/edit#gid=0",
+                                   sheet="Parkrunlist")
+
+ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+      .shiny-output-error-validation {
+        color: #ff0000;
+        font-size:20px;
+        font-weight: bold;
+      }
+    "))
+  ),
+  tags$head(tags$script('
+                                var dimension = [0, 0];
+                                $(document).on("shiny:connected", function(e) {
+                                    dimension[0] = window.innerWidth;
+                                    dimension[1] = window.innerHeight;
+                                    Shiny.onInputChange("dimension", dimension);
+                                });
+                                $(window).resize(function(e) {
+                                    dimension[0] = window.innerWidth;
+                                    dimension[1] = window.innerHeight;
+                                    Shiny.onInputChange("dimension", dimension);
+                                });
+                            ')),
+  # Application title
+  titlePanel("Mutual NENDY"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("adapat", 
+                  "ADAPAT members", 
+                  choices = c("Adam"= 7232608,
+                              "Alex"= 4087050,
+                              "Andy"= 7402459,
+                              "Ella"= 7470572,
+                              "Frankie"= 4458732,
+                              "Grace"=7554791,
+                              "Jonny"= 593256,
+                              "Katie"= 7526532,
+                              "Leah"= 5700545,
+                              "Luke"=2407768,
+                              "Max"=582473,
+                              "Phil"= 7266141,
+                              "Rachel"= 1539187,
+                              "Rob"= 5349926,
+                              "Seb"= 493595,
+                              "Tom A"= 5243446,
+                              "Tom C"= 1939393
+                  ),
+                  multiple = T
+                  
+      ),
+      selectInput("parkrun", "Home parkrun", choices = names$Short , selected = "South Manchester"),
+      numericInput("distance", "Maximum Travel Distance (miles)",value=20,min = 0),
+      actionButton("go", "Go")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      # dataTableOutput("test"),
+      # textOutput("test2"),
+      dataTableOutput("mnendy")%>% withSpinner(color="#0dc5c1")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  prdata=eventReactive(input$go, {
+    validate(need(length(input$adapat)>0, "Pick someone" ))
+    
+    #this currently needs to be commented out as the web scraper isn't working yet
+    
+    # adapat2=data.frame("Barcode"=input$adapat) %>% merge(adapat, by="Barcode")
+    # parkruns=tribble(~Name,~Event,~`Run Date`,~`Run Number`,~Pos,~Time,~AgeGrade,~`PB?`)
+    # 
+    # for(i in 1:nrow(adapat2))
+    # {
+    #   url=adapat2$url[i]
+    #   link=getURL(url)
+    #   table=readHTMLTable(link)[[3]] %>% mutate(Name=adapat2$Name[i],
+    #                                             Barcode=adapat2$Barcode[i])
+    #   parkruns=parkruns %>% rbind.data.frame(table)
+    #   # Sys.sleep(5)
+    # }
+
+    # adapat2
+    # parkruns
+    
+
+    
+    parkruns %>%
+      filter(Barcode %in% input$adapat) %>%
+      dplyr::select(Event)  %>% unlist()
+  })
+  
+  output$mnendy=renderDataTable({
+    `%notin%`=Negate(`%in%`)
+    validate(need(length(input$adapat)>0, "" ))
+    Data %>%
+      merge(names %>% dplyr::select(Name, Short), by.x="Home", by.y="Name") %>%
+      filter(
+        Short.y==input$parkrun,
+        Short.x %notin% prdata(),
+        miles<=input$distance
+      ) %>%
+      # %>% 
+      #   mutate(check=Short.x %notin% prdata())
+      dplyr::select(Short.x, miles, km) %>% 
+      rename("Parkrun"="Short.x") %>% 
+      mutate(miles=round(miles,1),
+             km=round(km,1))
+      
+  })
+  
+  output$test=renderDataTable({
+    prdata()
+  })
+  
+  output$test2=renderText({
+    url=adapat$url[15]
+      link=getURL(url)
+      link
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ R/PRdatadownload.R
@@ -0,0 +1,97 @@
+library(XML)
+library(tidyverse)
+library(RCurl)
+library(readxl)
+library(openxlsx)
+library(beepr)
+library(chron)
+library(tibble)
+library(chron)
+library(googlesheets4)
+# library(shinyjs)
+options(gargle_oauth_cache = ".secrets")
+gs4_auth(
+  cache = ".secrets",
+  email = "sebastiangbate@gmail.com"
+)
+adapat=tribble(~Name, ~Barcode,
+               "Seb", 493595,
+               "Rachel", 1539187,
+               "Tom C", 1939393,
+               "Frankie", 4458732,
+               "Alex", 4087050,
+               "Rob", 5349926,
+               "Andy", 7402459,
+               "Luke", 2407768,
+               "Jonny", 593256,
+               "Adam", 7232608,
+               "Katie", 7526532,
+               "Max", 582473,
+               "Leah", 5700545,
+               "Grace", 7554791,
+               "Ella", 7470572,
+               "Tom A", 5243446,
+               "Phil", 7266141
+)%>% arrange(Name) %>%  mutate(rm=paste("|", Name, "|[A",Barcode,"](https://www.parkrun.org.uk/parkrunner/",Barcode,"/all/)|",sep=""),
+                               url=paste("https://www.parkrun.org.uk/parkrunner/",Barcode,"/all/",sep=""))
+
+
+
+write_lines(c("|Name|Barcode|","|-|-|", adapat$rm), "README.md")
+
+
+parkruns=tribble(~Name,~Barcode,~Event,~`Run Date`,~`Run Number`,~Pos,~Time,~AgeGrade,~`PB?`)
+
+for(i in 1:nrow(adapat))
+{
+  url=adapat$url[i]
+  link=getURL(url)         
+  table=readHTMLTable(link)[[3]] %>% mutate(Name=adapat$Name[i],
+                                            Barcode=adapat$Barcode[i])
+  parkruns=parkruns %>% rbind.data.frame(table)
+  Sys.sleep(10)
+}
+beepr::beep()
+
+parkruns=parkruns %>% mutate(Time2=case_when(str_length(Time)==5~chron(times=paste("00:",Time, sep="")),
+                                             T~chron(times=paste(Time))))
+parkruns %>% count(Event) %>% arrange(-n) 
+
+x=parkruns %>% count(Name, Event) %>% pivot_wider(
+  names_from = "Name", values_from = n) %>% 
+  merge(parkruns %>% count(Event))%>% arrange(-n) %>% select(-n)
+
+
+sheet_write(x, ss="https://docs.google.com/spreadsheets/d/1Bv_LGrOK6leEFV76OlHPsLX8CvM17CmEhQ2_JzZg5YI/edit#gid=0",
+            sheet="Parkruns")
+
+y=parkruns %>% group_by(Name, Event) %>% slice_min(Time2, with_ties = F) %>% 
+  mutate(Time=substr(as.character(Time2), 9-str_length(Time),9)) %>% 
+  dplyr::select(Name, Event, Time) %>% 
+  pivot_wider(names_from = "Name", values_from = Time) %>% 
+  merge(parkruns %>% count(Event))%>% arrange(-n) %>% select(-n)
+
+sheet_write(y, ss="https://docs.google.com/spreadsheets/d/1Bv_LGrOK6leEFV76OlHPsLX8CvM17CmEhQ2_JzZg5YI/edit#gid=0",
+            sheet="PBs")
+
+
+parkruns2=parkruns %>% group_by(Event, Name, Barcode) %>% slice_min(`Run Date`, with_ties = F) %>% ungroup() %>% dplyr::select(Event, Name, Barcode)
+sheet_write(parkruns2, ss="https://docs.google.com/spreadsheets/d/1Bv_LGrOK6leEFV76OlHPsLX8CvM17CmEhQ2_JzZg5YI/edit#gid=0",
+            sheet="Parkrunlist")
+
+save(adapat, file="ADAparkrun/adapat.Rda" )
+
+save(parkruns, parkruns2, file="Data/Cache.Rda")
+
+library(rvest)
+library(httr)
+i=15
+
+# url=adapat$url[i]
+# download.file(url, destfile = "junk/scrapedpage.html", quiet=TRUE)
+# rvest::read_html(url)
+# 
+# session(url)
+# con=curl::curl(url)
+# 
+# %>% read_html()
